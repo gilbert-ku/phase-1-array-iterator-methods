51a30220304238c888ed78c43068b1af81c8dd15
gilbert-ku <gilbert45ku@gmail.com>
2023-06-19 12:47:04 +0300

    add js file

---
+++ script.js
@@ -0,0 +1,75 @@
+const users = [
+  {
+    firstName: "Niky",
+    lastName: "Morgan",
+    favoriteColor: "Blue",
+    favoriteAnimal: "Jaguar",
+    personalQuote: "You're never too old to learn something new",
+  },
+  {
+    firstName: "Tracy",
+    lastName: "Lum",
+    favoriteColor: "Yellow",
+    favoriteAnimal: "Penguin",
+    personalQuote: "I just got lost in thought - it was unfamiliar territory",
+  },
+  {
+    firstName: "Josh",
+    lastName: "Rowley",
+    favoriteColor: "Blue",
+    favoriteAnimal: "Penguin",
+    personalQuote: "Always remember you’re unique, just like everyone else",
+  },
+  {
+    firstName: "Kate",
+    lastName: "Travers",
+    favoriteColor: "Red",
+    favoriteAnimal: "Jaguar",
+    personalQuote: "Behind every great man is a woman rolling her eyes",
+  },
+  {
+    firstName: "Avidor",
+    lastName: "Turkewitz",
+    favoriteColor: "Blue",
+    favoriteAnimal: "Penguin",
+    personalQuote:
+      "You don’t have to see the whole staircase, just take the first step",
+  },
+  {
+    firstName: "Drew",
+    lastName: "Price",
+    favoriteColor: "Yellow",
+    favoriteAnimal: "Elephant",
+    personalQuote:
+      "Failure is not the opposite of success: it’s part of success",
+  },
+];
+
+//************to get firstname and the last name */
+// function firstNamePrinter(collection) {
+//     for (const user of collection) {
+//         console.log(user.firstName);
+//     }
+// }
+
+// firstNamePrinter(users);
+
+// lastNamePrinter = (collection) => {
+//     for (const users of collection) {
+//         console.log(users.lastName);
+//     }
+// }
+
+// lastNamePrinter(users);
+
+
+//**********to get one user in an arrey */
+// function blueFilter(collection) {
+//     for (const user of collection) {
+//       if (user.favoriteColor === "Blue") {
+//         console.log(user.firstName);
+//       }
+//     }
+//   }
+  
+//   blueFilter(users);
